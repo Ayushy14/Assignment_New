c62e7b162792dbc814aef7e0f72f16387182682c
Ayushy14 <Ayushy@appliedis.com>
2018-03-16 20:36:00 +0530

    Add project files.

---
+++ Assignment_New/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Assignment_New.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Inserting Players Information</title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2><i>Please enter the players details to either populate the database or delete the contents.</i></h2>
+     
+        <table class="style1">
+               <h3>
+            <tr>
+                <td>Players Name:</td>
+                <td>
+                    <asp:TextBox ID="PlayerName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Players Team</td>
+                <td>
+                    <asp:TextBox ID="PlayerTeam" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>ICC Ranking</td>
+                <td>
+                    <asp:TextBox ID="ICCRanking" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Best Score</td>
+                <td>
+                    <asp:TextBox ID="BestScore" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+                   
+          </h3>
+        </table>
+         <asp:Button ID="Button1" runat="server" Text="Update" onclick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Delete" onclick="Button2_Click" />
+          
+
+    </div>
+    </form>
+</body>
+</html>
+
---
+++ Assignment_New/WebForm1.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Assignment_New
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ExecuteInsert(PlayerName.Text, PlayerTeam.Text, ICCRanking.Text, BestScore.Text);
+            Response.Write("Record was successfully added!");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ExecuteDelete(PlayerName.Text, PlayerTeam.Text, ICCRanking.Text, BestScore.Text);
+        }
+
+        public void ExecuteDelete(string PlayerName, string PlayerTeam, string ICCRanking, string BestScore)
+        {
+            SqlConnection conn = new SqlConnection(GetConnectionString());
+            string sql1 = "DELETE FROM PlayerInfo WHERE PlayerName=@PlayerName";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql1, conn);
+                //SqlDataAdapter adapter = new SqlDataAdapter();
+                //adapter.DeleteCommand = new SqlCommand(sql1, conn);
+                //adapter.DeleteCommand.ExecuteNonQuery();
+                cmd.Parameters.AddWithValue("@PlayerName", PlayerName);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                Response.Write("The record has been deleted successfully");
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                string msg = "Delete Error:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+            public string GetConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["MyConsString"].ConnectionString;
+        }
+        public void ExecuteInsert(string PlayerName, string PlayerTeam, string ICCRanking, string BestScore)
+        {
+            SqlConnection conn = new SqlConnection(GetConnectionString());
+            string sql = "INSERT INTO PlayerInfo(PlayerName,PlayerTeam,ICCRanking,BestScore) VALUES " + 
+                "(@PlayerName,@PlayerTeam,@ICCRanking,@BestScore)";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlParameter[] param = new SqlParameter[4];
+
+                param[0] = new SqlParameter("@PlayerName",SqlDbType.VarChar,50);
+                param[1] = new SqlParameter("@PlayerTeam",SqlDbType.VarChar,50);
+                param[2] = new SqlParameter("@ICCRanking",SqlDbType.VarChar,50);
+                param[3] = new SqlParameter("@BestScore",SqlDbType.VarChar,50);
+
+
+                param[0].Value = PlayerName;
+                param[1].Value = PlayerTeam;
+                param[2].Value = ICCRanking;
+                param[3].Value = BestScore;
+
+                for (int i = 0; i < param.Length; i++)
+                {
+                    cmd.Parameters.Add(param[i]);
+                }
+
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                string msg = "Insert Error:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
